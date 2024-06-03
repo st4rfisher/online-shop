cdbfdd494726c9a0118e7c996ae93b2085b0cc00
starfisher <supremeeworker@gmail.com>
2024-06-03 16:20:31 +0300

    remove unused files, add images

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {
+    
+    return {
+
+    }
+  },
+  render() {
+    return (
+        <>
+            <span class='mb-5 text-slate-200'>ewrwer</span>
+        </>
+    )
+  },
+})
+
